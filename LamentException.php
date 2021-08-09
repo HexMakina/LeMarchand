e914fc6e5ded9395f4ea0e3eed19bf2897766c4e
HexMakina <20891495+HexMakina@users.noreply.github.com>
2021-08-09 23:01:21 +0200

    var_export call params
    Returns the variable representation when the return parameter is used and evaluates to true

--- LamentException.php
+++ LamentException.php
@@ -7,7 +7,7 @@ class LamentException extends \Exception implements ContainerExceptionInterface
 {
   public function __construct($configuration)
   {
-    $configuration = json_encode(var_export($configuration));
+    $configuration = json_encode(var_export($configuration, true));
     return parent::__construct("HellBound Error using '$configuration'");
   }  
 }
