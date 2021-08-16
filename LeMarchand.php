fdc5ba23fb59797083cc09ea379cb3fb6f6e7e70
sam <sammy@hexmakina.be>
2021-08-16 19:35:17 +0200

    simplified get() with submethods get_settings() and get_instance()

--- LeMarchand.php
+++ LeMarchand.php
@@ -1,5 +1,5 @@
-<?php 
- 
+<?php
+
 namespace HexMakina\LeMarchand;
 
 use \Psr\Container\ContainerInterface;
@@ -7,12 +7,12 @@ use \Psr\Container\ContainerInterface;
 class LeMarchand implements ContainerInterface
 {
   private $configurations = [];
-  
+
   public function __construct($settings)
   {
     $this->configurations['settings'] = $settings;
   }
-  
+
   public function __debugInfo() : array
   {
     $dbg = get_object_vars($this);
@@ -20,67 +20,79 @@ class LeMarchand implements ContainerInterface
 
     return $dbg;
   }
-  
+
   public function register($configuration, $instance)
   {
     $this->configurations[$configuration] = $instance;
   }
-  
+
   public function has($configuration)
   {
     return isset($this->configurations[$configuration]);
   }
-  
+
+  private function get_instance($class)
+  {
+    $rc = new \ReflectionClass($class);
+
+    $construction_args = [];
+    $instance = null;
+    if(!is_null($rc->getConstructor()))
+    {
+      foreach($rc->getConstructor()->getParameters() as $param)
+      {
+        $construction_args []= $this->get($param->getType().'');
+      }
+      $instance = $rc->newInstanceArgs($construction_args);
+    }
+    else
+      $instance = $rc->newInstanceArgs();
+
+    if($rc->hasMethod('set_container'))
+      $instance->set_container($this);
+
+    return $instance;
+  }
+
+
+  private function get_settings($setting)
+  {
+    $ret = $this->configurations;
+
+    //dot based hierarchy, parse and climb
+    foreach(explode('.', $setting) as $k)
+    {
+      if(!isset($ret[$k]))
+        throw new ConfigurationException($setting);
+      $ret = $ret[$k];
+    }
+
+    return $ret;
+  }
+
   public function get($configuration)
   {
-    
+
     if(!is_string($configuration))
       throw new LamentException($configuration);
 
     if($this->has($configuration))
       return $this->configurations[$configuration];
 
-    
     // fallbacks
+    // 1. configuration data
+    // 2. creating instances
+    
     if(preg_match('/^settings\./', $configuration, $m) === 1)
     {
-      $ret = $this->configurations;
-      foreach(explode('.', $configuration) as $k)
-      {
-        if(!isset($ret[$k]))
-          throw new ConfigurationException($configuration);
-        $ret = $ret[$k];
-      }
-
-      return $ret;
+      return $this->get_settings($configuration);
     }
     elseif(class_exists($configuration)) // auto create instances
     {
-      
-      $rc = new \ReflectionClass($configuration);
-
-      $construction_args = [];
-      $instance = null;
-      if(!is_null($rc->getConstructor()))
-      {
-        foreach($rc->getConstructor()->getParameters() as $param)
-        {
-          $construction_args []= $this->get($param->getType().'');
-        }
-        $instance = $rc->newInstanceArgs($construction_args);
-      }
-      else 
-        $instance = $rc->newInstanceArgs();
-
-      if($rc->hasMethod('set_container'))
-        $instance->set_container($this);
-      
-      return $instance;
+      return $this->get_instance($configuration);
     }
 
     throw new ConfigurationException($configuration);
   }
 
 }
-
-
