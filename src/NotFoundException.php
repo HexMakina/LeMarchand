0447025947fcf7e1ca82b7e72ad300d7e05a479f
sam <sammy@hexmakina.be>
2022-08-25 10:05:06 +0200

    php8: typed arguments

--- src/NotFoundException.php
+++ src/NotFoundException.php
@@ -6,7 +6,7 @@ use Psr\Container\NotFoundExceptionInterface;
 
 class NotFoundException extends \Exception implements NotFoundExceptionInterface
 {
-    public function __construct($configuration)
+    public function __construct(String $configuration)
     {
         parent::__construct("Unkown configuration '$configuration'");
     }
